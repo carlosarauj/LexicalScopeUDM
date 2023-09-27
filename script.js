3b7c2f9a72d50f9ca786015bef2762d85a088a5a
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-09-27 20:08:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* let nome = 'carlos'
+
+function falaNome(){
+    console.log(nome)
+}
+
+
+falaNome() */
+
+let nome = 'carlos'
+
+function falaNome(){
+    let nome = 'araujo'
+    console.log(nome)
+}
+
+function usaNome(){
+    let nome = 'otavio'
+    falaNome()
+}
+
+
+usaNome()
